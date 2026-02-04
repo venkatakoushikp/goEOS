c739a41a46910b51c57f15ba774b17c54c053324
venkatakoushikp <venkata.koushik@arista.com>
2026-02-04 16:36:10 +0530

    Update Banner

--- utils/config.go
+++ utils/config.go
@@ -13,33 +13,44 @@ package utils
 
 import (
 	"log"
+	"math"
+	"math/rand"
+	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/ssh"
 )
 
-func singleString(commands []string) (string) {
+const (
+	CommandsNeeded = "enable\nconfigure terminal\n+configure session goEOS-1493101506"
+)
 
-	// Append enable and Config commands to the final string 
+
+func commandParser(commands []string) (string) {
+
 	// ssh.CombinedOutput() expects a string. However we need to maintain the session to be able to run multiple commands.
-	// Hence we append everything into a single strinf and then return it to be used as command.
+	// Hence we append everything into a single string and then return it to be used as command.
+	// Adds CommandsNeeded Const for the header to ensure Config mode is entered.
+	// Also makes sure a new config session is created and the command is run within that session.
+	// If any errors are thrown, just exit the config session 
+	// If not errors, commit the config session.
 
+	configSession := "configure session goEOS-" + strconv.Itoa(rand.Intn( int(math.Pow(2,31)-1) ))
+	log.Println("Config Session Created :", configSession)
+	commands = append(commands, configSession)
+	return CommandsNeeded+strings.Join(commands, "\n")
 
-	var result string = "enable\nconfig\n"
-	return result+strings.Join(commands, "\n")
 }
 
 
 func Config(Commands []string, Client *ssh.Client) (error){
-
-	
 
 	session, err := Client.NewSession()
 	if err !=nil {
 		log.Println("Error while creating a new session :", err)
 	}
 
-	_, err = session.CombinedOutput(singleString(Commands))
+	_, err = session.CombinedOutput(commandParser(Commands))
 	if err!=nil{
 		return err
 	}
@@ -49,12 +60,6 @@ func Config(Commands []string, Client *ssh.Client) (error){
 
 }
 
+func validateSession() {
 
-func CreateConfigSession ():
-/*
-
-Creates a new config session where the config commands are run.
-If there are no errors, commit the session
-If there are error, throw those errors 
-
-*/
+}
